7b4d1102f9d9833bea4848c0dd4340076f19d008
AnuragDharmadhikari <106768147+AnuragDharmadhikari@users.noreply.github.com>
2024-02-24 12:48:26 +0530

    add czll to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -63,7 +64,11 @@ const PostPage = () => {
       <div
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
-      ></div>
+      >
+      </div>
+      <div className=" max-w-4xl mx-auto w-full">
+        <CallToAction/>
+      </div>
     </main>
   );
 };
