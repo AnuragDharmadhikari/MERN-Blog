20fef9669210dc044bd6c16d197d74e12d0bbeb4
AnuragDharmadhikari <106768147+AnuragDharmadhikari@users.noreply.github.com>
2024-02-28 14:32:27 +0530

    projects section completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className=" min-h-screen flex items-center justify-center">
+      <div className=" max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className=" text-3xl font-semibold text-center my-7">About Anurag's Blog</h1>
+          <div className=" flex text-md text-gray-500 flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              aspernatur excepturi facilis dolore voluptatem ex quibusdam earum
+              autem, ducimus sunt cumque labore laborum sit commodi veniam. Rem
+              enim fuga asperiores!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              aspernatur excepturi facilis dolore voluptatem ex quibusdam earum
+              autem, ducimus sunt cumque labore laborum sit commodi veniam. Rem
+              enim fuga asperiores!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              aspernatur excepturi facilis dolore voluptatem ex quibusdam earum
+              autem, ducimus sunt cumque labore laborum sit commodi veniam. Rem
+              enim fuga asperiores!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
