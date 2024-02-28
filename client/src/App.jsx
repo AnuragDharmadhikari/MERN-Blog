66a76b9c6f4ab8eacffbaf9d2c31e49ad29c9847
AnuragDharmadhikari <106768147+AnuragDharmadhikari@users.noreply.github.com>
2024-02-28 14:20:47 +0530

    add search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from "./pages/createPost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScroolToTop from "./components/ScroolToTop";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-in" element={<Signin />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,8 +11,8 @@ import {
   NavbarToggle,
   TextInput,
 } from "flowbite-react";
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
@@ -21,15 +21,25 @@ import { signOutSuccess } from "../redux/user/userSlice.js";
 
 const Header = () => {
   const path = useLocation().pathname;
+  const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   const handelSignOut = async () => {
     try {
       const res = await fetch("/api/user/signout", {
         method: "POST",
       });
-      const data =await res.json();
+      const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
       } else {
@@ -38,6 +48,15 @@ const Header = () => {
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+
+    navigate(`/search?${searchQuery}`);
   };
 
   return (
@@ -51,12 +70,14 @@ const Header = () => {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
